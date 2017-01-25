3bdec64d3d0bcf3f2b2a0b71fce3d3bc9ab449bd
TomoTomoM <tomohiromaeda@dhcp-18-111-39-222.dyn.mit.edu>
2017-01-25 01:13:45 -0500

    first commit

---
+++ Assets/Animated Wasps Pack/AttackingBee/Sctipt/BeeBehavior1.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class BeeBehavior1 : MonoBehaviour {
+
+	public GameObject stinger;
+
+
+	public float speed;
+	public float localXRange;
+	public float localYRange;
+	public Animator anim;
+	public float stingerInterval;
+
+	private GameObject Player;
+	private Transform player;
+	private Vector3 firstPosition;
+//	private float firstX;
+//	private float firstY;
+//	private float firstZ;
+	private float deltaX;
+	private float deltaY;
+	private float deltaZ;
+
+//	private float maxLocalX;
+//	private float minLocalX;
+//	private float maxLocalY;
+//	private float minLocalY;
+	private float deltaTime = 0f;
+	private float animTimeTracker = 0f;
+
+	private bool stingerShooted = false;
+	private int stingerCounter = 0;
+
+	public Vector3 stingerOffset = new Vector3(0.1f,0.0f,0.4f);
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		player = Player.transform; //player is just transform of game object "Player"
+		firstPosition = transform.localPosition;
+		LookAtPlayer (); //face the player
+		//define the area the bee can move
+		DefineMovingRange();
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		deltaTime += Time.deltaTime;
+		animTimeTracker += Time.deltaTime;
+		GetXYDisplacement ();
+		transform.Translate(deltaX, deltaY, 0);
+		LookAtPlayer ();
+		transform.Rotate (0, 10, 0);
+		ShootStinger ();
+		StartCoroutine ("CreateStinger");
+		stingerShooted = false;
+	}
+
+	void LookAtPlayer(){
+		if(player != null)
+		{
+			transform.LookAt(player);
+		}
+	}
+
+	void DefineMovingRange(){
+//		maxLocalX = transform.localPosition.x + localXRange / 2.0f;
+//		minLocalX = transform.localPosition.x - localXRange / 2.0f;
+//		maxLocalY = transform.localPosition.y + localYRange / 2.0f;
+//		minLocalY = transform.localPosition.y - localYRange / 2.0f;
+//		print (maxLocalX);
+//		print (minLocalX);
+	}
+
+	void GetXYDisplacement(){
+		// if n second passed since last change in movement, change it
+		if (deltaTime > 2.0f) {
+			deltaX = Random.Range (-speed, speed);
+			deltaY = Random.Range (-speed, speed);
+			deltaZ = Random.Range (-speed, speed);
+			deltaTime = 0.0f;
+		}	
+		// if bee is far from its starting position, change direction
+		if (Vector3.Distance (firstPosition, transform.position) > 1) {
+			deltaX = -deltaX;
+			deltaY = -deltaY;
+			deltaZ = -deltaZ;
+		}
+
+//		if (transform.localPosition.x > maxLocalX) {
+//			deltaX = Random.Range (speed, 0.0f); //not -speed because +x direction of bee is -x direction of the player
+//			deltaTime = 0.0f;
+//			print ("hit x max");
+//		}
+//		if (transform.localPosition.x < minLocalX) {
+//			deltaX = Random.Range (-speed, 0.0f);	//not +speed because -x direction of bee is +x direction of the player
+//			deltaTime = 0.0f;
+//			print ("hit x min");
+//		}
+//		if (transform.localPosition.y > maxLocalY) {
+//			deltaY = Random.Range (-speed*0.5f, 0.0f);
+//			deltaTime = 0.0f;
+//			print("hit y max");
+//		}
+//		if (transform.localPosition.y < minLocalY) {
+//			deltaY = Random.Range (speed*0.5f, 0.0f);
+//			deltaTime = 0.0f;
+//			print ("change direction");
+//		}
+//		if (deltaTime > 3.0f) {
+//			deltaX = Random.Range (-speed, speed);
+//			deltaY = Random.Range (-speed*0.5f, speed);
+//			deltaTime = 0.0f;
+//		}
+	}
+		
+	void ShootStinger(){
+		if (animTimeTracker > stingerInterval) {
+			anim.Play ("Hit1");
+			stingerShooted = true;
+			print ("shoot stinger");
+			animTimeTracker = 0.0f;
+		}
+	}
+		
+
+	IEnumerator CreateStinger (){
+		if (stingerShooted == true) {
+			yield return new WaitForSeconds (0.3f);
+			Instantiate (stinger, transform.position + stingerOffset, transform.rotation);
+		}
+	}
+
+
+}
---
+++ Assets/Animated Wasps Pack/AttackingBee/Sctipt/StingerBehavior.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StingerBehavior : MonoBehaviour {
+
+	//public Transform player;
+	//public Transform controllerPointer;
+	public float speed ;
+	public Vector3 playerHoldingOffset;
+	//public float shootingSpeed = 1000.0f;
+
+	//public Vector3 pointer;
+	private GameObject player;
+	private GameObject pointer;
+	private Vector3 shootingDestination;
+
+	//private Rigidbody rb;
+	//private GameObject controllerPointer;
+
+	private int stingerState = 0; //0:coming, 1:getting 2:holding, 2:shooting
+
+	// Use this for initialization
+	void Start () {
+		//rb = GetComponent<Rigidbody>();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		pointer = GameObject.FindGameObjectWithTag ("ControllerPointer");
+		//controllerPointer = GameObject.FindGameObjectWithTag ("ControllerPointer");
+		transform.parent = player.transform; //set transform parent to the player so that stinger does not move with bee
+		LookAtPlayer ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (stingerState == 0) {
+			comingUpdate ();
+		}
+		if (stingerState == 1) {
+			stoppingUpdate ();
+		}
+		if (stingerState == 2) {
+			gettingUpdate ();
+		}
+		if (stingerState == 3) {
+			holdingUpdate ();
+		}
+		if (stingerState == 4) {
+			shootingUpdate ();
+		}
+	}
+
+
+	void comingUpdate(){
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position, step); //move stinger to the player with constant speed
+			if (Input.GetKeyDown("1")){
+				stingerState = 1;
+			}
+		if (Vector3.Distance(player.transform.position,transform.position) < 0.1f){
+			print("Stinger Destroyed");
+			Destroy(this.gameObject);
+		}
+	}
+
+	void stoppingUpdate(){
+		//transform.position = transform.position;
+		if (GvrController.Gyro[0] > 0.5){
+			stingerState = 2;
+		}
+				if (Input.GetKeyDown("2")){
+					stingerState = 2;
+				}
+	}
+
+	void gettingUpdate(){
+		float step = speed * 10.0f * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position + playerHoldingOffset, step); //move stinger to the player with constant speed
+		if (Vector3.Distance(player.transform.position+playerHoldingOffset,transform.position) < 0.3){
+			stingerState = 3;
+		}
+	}
+	void holdingUpdate(){
+		transform.position = player.transform.position + playerHoldingOffset; //fix position of the stinger near the player. 
+		transform.LookAt(pointer.transform.position );
+		//bee should be replaced by controller pointer
+		transform.Rotate (90, 0, 0);
+				if (Input.GetKeyDown("3")){
+					shootingDestination = 2*pointer.transform.position-transform.position;
+					stingerState = 4;
+				}
+	}
+	void shootingUpdate(){
+		//use add force instead
+		float step = speed * 10.0f * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, shootingDestination , step); //move stinger to the player with constant speed
+		//rb.AddForce ((shootingDestination-transform.position) * speed);
+		//print ("shooted");
+		Destroy(this.gameObject,1);
+	}
+
+
+
+	void LookAtPlayer(){
+		if(player != null)
+		{
+			transform.LookAt(player.transform); 
+			transform.Rotate (90, 0, 0);
+		}
+	}
+
+	public void StoppingStinger(){
+		stingerState += 1;
+	}
+
+	public void ShootingStinger(){
+		if (stingerState == 1) {
+			stingerState = 0;
+		} else {
+			shootingDestination = 2*pointer.transform.position-transform.position;
+			stingerState = 4;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Bee"))
+		{
+			if (stingerState == 4) {
+				print ("Bee shooted");
+				other.gameObject.SetActive (false);
+			}
+		}
+		if (other.gameObject.CompareTag("Stinger")){
+			
+			Destroy(other.gameObject,1.0f);
+			Destroy(this.gameObject, 1.0f);
+		}
+	}
+
+}
