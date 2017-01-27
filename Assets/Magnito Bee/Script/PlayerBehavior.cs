83c0d8cdff8b18f95fc9aee71fec98731b5d5add
TomoTomoM <tomohiromaeda@dhcp-18-111-92-184.dyn.mit.edu>
2017-01-26 19:39:10 -0500

    Main Scene + Main Menu done

---
+++ Assets/Magnito Bee/Script/PlayerBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("5")) {
+			Application.LoadLevel ("MainMenu");
+		}
+	}
+}
